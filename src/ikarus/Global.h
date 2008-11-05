a706d561274cf5f50112a55e2ad6b673db9944b8
John Bartholomew <john.waffles@gmail.com>
2008-11-05 17:54:28 +0000

    Added a rect<T> struct that has a top-left vec2<T> and a size vec2<T>

--- src/ikarus/Global.h
+++ src/ikarus/Global.h
@@ -41,15 +41,40 @@
 #include "scopedenum.h"
 #include "murmurhash.h"
 
+namespace vmath
+{
+	template <typename T>
+	struct rect
+	{
+		rect(): topLeft(T(0), T(0)), size(T(0), T(0)) {}
+
+		explicit rect(const vec2<T> &v): topLeft(v), size(T(0), T(0)) {}
+		explicit rect(const vec2<T> &v0, const vec2<T> &v1): topLeft(v0), size(v1) {}
+		explicit rect(T left, T top): topLeft(left, top), size(T(0), T(0)) {}
+		explicit rect(T left, T top, T width, T height): topLeft(left, top), size(width, height) {}
+
+		bool operator==(const rect<T> &b) const
+		{ return (topLeft == b.topLeft) && (size == b.size); }
+		bool operator!=(const rect<T> &b) const
+		{ return !(*this == b); }
+
+		vec2<T> topLeft;
+		vec2<T> size;
+	};
+}
+
 typedef vmath::vec2<int> vec2i; // useful for screen/pixel coordinates
+typedef vmath::rect<int> recti;
 
 typedef vmath::vec2<double> vec2d;
 typedef vmath::vec3<double> vec3d;
 typedef vmath::vec4<double> vec4d;
+typedef vmath::rect<double> rectd;
 
 typedef vmath::vec2<float> vec2f;
 typedef vmath::vec3<float> vec3f;
 typedef vmath::vec4<float> vec4f;
+typedef vmath::rect<float> rectf;
 
 typedef vmath::mat2<double> mat2d;
 typedef vmath::mat3<double> mat3d;
